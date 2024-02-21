7dcdbc756fb55635f3cec3163d4a8c9a23e5ee23
Seo Chanhyeok <popcorn@g.hongik.ac.kr>
2024-02-21 15:28:51 +0900

    feature: CICD 파이프라인 테스트

--- src/test/java/com/example/repick/RepickApplicationTests.java
+++ src/test/java/com/example/repick/RepickApplicationTests.java
@@ -1,7 +1,6 @@
 package com.example.repick;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
