aafe6bff99d99fb2b454962da34ad5558f724ad9
Seo Chanhyeok <popcorn@g.hongik.ac.kr>
2024-02-21 17:10:39 +0900

    Feature/cicd deployment (#5)
    * feature: CICD 파이프라인 테스트
    
    * feature: CICD 파이프라인 테스트
    
    * feature: CICD 파이프라인 테스트
    
    * feature: CICD 파이프라인 테스트
    
    * feature: CICD 파이프라인 테스트
    
    * feature: CICD 파이프라인 테스트
    
    * feature: CICD 파이프라인 테스트
    
    * feature: CICD 파이프라인 테스트 완료

--- src/test/java/com/example/repick/RepickApplicationTests.java
+++ src/test/java/com/example/repick/RepickApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.repick;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RepickApplicationTests {
-
 
 	@Test
 	void contextLoads() {
