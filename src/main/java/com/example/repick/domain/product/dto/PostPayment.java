9f4214b385628cdd972dd4650e45357bc14e9014
Jeonghyun Lee <hyeon00@ewhain.net>
2024-04-24 15:17:30 +0900

    refactor: delete unused field in dto

--- src/main/java/com/example/repick/domain/product/dto/PostPayment.java
+++ src/main/java/com/example/repick/domain/product/dto/PostPayment.java
@@ -8,6 +8,5 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record PostPayment(
         @Schema(description = "결제 고유번호") String iamportUid,
         @Schema(description = "주문 아이디") String merchantUid,
-        @Schema(description = "결제 금액") Long paymentAmount,
         Address address) {
 }
