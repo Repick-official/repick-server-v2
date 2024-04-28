1a0b28d0dcd87b788e59b2dc1ac3e038de4a59a8
muhsroom1324 <mushroom1324@naver.com>
2024-04-28 23:29:44 +0900

    feature: add refresh token api

--- src/main/java/com/example/repick/domain/user/service/UserService.java
+++ src/main/java/com/example/repick/domain/user/service/UserService.java
@@ -22,7 +22,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
@@ -61,7 +60,7 @@ public class UserService {
 
     }
 
-    public Boolean registerProfile(MultipartFile profileImage) throws IOException {
+    public Boolean registerProfile(MultipartFile profileImage) {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
 
         User user = userRepository.findByProviderId(email)
