89359ccfa8670f911ec21594e0b8567259866183
Chanhyeok Seo <popcorn@g.hongik.ac.kr>
2024-04-30 09:04:56 +0900

    Feature/refresh token (#44)
    * feature: add refresh token api
    
    * feature: add refresh token api

--- src/main/java/com/example/repick/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/repick/global/error/exception/ErrorCode.java
@@ -15,6 +15,8 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "C004", "존재하지 않는 사용자입니다."),
     INVALID_REQUEST_ERROR(400, "C005", "잘못된 요청입니다."),
     INVALID_CATEGORY_ID(400, "C006", "존재하지 않는 카테고리 ID입니다."),
+    // Security
+    TOKEN_EXPIRED(401, "S001", "토큰이 만료되었습니다."),
     // Category
     INVALID_CATEGORY_NAME(400, "P001", "존재하지 않는 카테고리 이름입니다."),
     IMAGE_UPLOAD_FAILED(500, "P002", "이미지 업로드에 실패했습니다."),
--- src/main/java/com/example/repick/global/jwt/JpaUserDetailsService.java
+++ src/main/java/com/example/repick/global/jwt/JpaUserDetailsService.java
@@ -17,8 +17,8 @@ public class JpaUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) {
-        User user = userRepository.findByProviderId(email).orElseThrow(
+    public UserDetails loadUserByUsername(String providerId) {
+        User user = userRepository.findByProviderId(providerId).orElseThrow(
                 () -> new CustomException(USER_NOT_FOUND)
         );
 
