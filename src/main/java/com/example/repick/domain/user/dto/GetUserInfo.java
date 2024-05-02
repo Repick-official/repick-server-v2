27771472004887af6cb99508c88c337af6f9d7a4
muhsroom1324 <mushroom1324@naver.com>
2024-05-02 13:19:42 +0900

    feature: return phone_number and size info on GET user

--- src/main/java/com/example/repick/domain/user/dto/GetUserInfo.java
+++ src/main/java/com/example/repick/domain/user/dto/GetUserInfo.java
@@ -8,7 +8,10 @@ public record GetUserInfo (
         @Schema(description = "닉네임", example = "테스트") String nickname,
         @Schema(description = "프로필 사진", example = "https://d1j8r0kxyu9tj8.cloudfront.net/files/1617616479Z1X6X1X1/profile_pic.jpg") String profileImage,
         @Schema(description = "푸시알림 허용 여부 ( true | false )", example = "true") Boolean pushAllow,
-        @Schema(description = "유저 등급", example = "루키콜렉터") String userClass
+        @Schema(description = "유저 등급", example = "아직 설정되지 않음") String userClass,
+        @Schema(description = "휴대폰 번호", example = "010-1234-5678") String phoneNumber,
+        @Schema(description = "상의 사이즈", example = "M") String topSize,
+        @Schema(description = "하의 사이즈", example = "M") String bottomSize
 ) {
     public static GetUserInfo of(User user) {
         return new GetUserInfo(
@@ -16,7 +19,10 @@ public record GetUserInfo (
                 user.getNickname(),
                 user.getProfileImage(),
                 user.getPushAllow(),
-                user.getUserClass().getValue()
+                user.getUserClass().getValue(),
+                user.getPhoneNumber(),
+                user.getTopSize(),
+                user.getBottomSize()
         );
     }
 }
--- src/main/java/com/example/repick/domain/user/entity/UserClass.java
+++ src/main/java/com/example/repick/domain/user/entity/UserClass.java
@@ -2,9 +2,9 @@ package com.example.repick.domain.user.entity;
 
 public enum UserClass {
 
-    ROOKIE_COLLECTOR(1, "루키콜렉터"),
-    BEGINNER_COLLECTOR(2, "초보콜렉터"),
-    PRO_COLLECTOR(3, "프로콜렉터");
+    ROOKIE_COLLECTOR(1, "TEST_BRONZE"),
+    BEGINNER_COLLECTOR(2, "TEST_SILVER"),
+    PRO_COLLECTOR(3, "TEST_GOLD");
 
     private final int id;
     private final String value;
