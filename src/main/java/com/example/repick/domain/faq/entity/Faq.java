d70f2b7eeb77d383642448108087950d6a998791
Jeonghyun Lee <hyeon00@ewhain.net>
2024-05-07 20:38:48 +0900

    chore: faq answer to TEXT

--- src/main/java/com/example/repick/domain/faq/entity/Faq.java
+++ src/main/java/com/example/repick/domain/faq/entity/Faq.java
@@ -1,10 +1,7 @@
 package com.example.repick.domain.faq.entity;
 
 import com.example.repick.global.entity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,6 +17,7 @@ public class Faq extends BaseEntity {
 
     private String question;
 
+    @Column(columnDefinition = "TEXT")
     private String answer;
 
     @Builder
