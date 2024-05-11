ececaf2b2a2acb0c23fbf3b9ab69e6bf197c5e25
Jeonghyun Lee <hyeon00@ewhain.net>
2024-05-12 04:24:03 +0900

    chore: delete unused import

--- src/main/java/com/example/repick/domain/product/repository/ProductRepositoryImpl.java
+++ src/main/java/com/example/repick/domain/product/repository/ProductRepositoryImpl.java
@@ -9,11 +9,8 @@ import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
