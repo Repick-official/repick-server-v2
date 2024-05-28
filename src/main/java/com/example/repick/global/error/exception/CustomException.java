0c3db2f8bc139b80f9487008a1a284480eed32eb
Jeonghyun Lee <hyeon00@ewhain.net>
2024-05-29 06:13:17 +0900

    chore: delete unnecessary parts

--- src/main/java/com/example/repick/global/error/exception/CustomException.java
+++ src/main/java/com/example/repick/global/error/exception/CustomException.java
@@ -16,8 +16,4 @@ public class CustomException extends RuntimeException {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
-    }
 }
