3e830690fd98fd620158de269386ded1a6fabd2e
JeongHyun Lee <86969518+hyunihs@users.noreply.github.com>
2024-06-01 11:38:21 +0900

    Fix: payment concurrency issue (#83)
    * refactor: fix concurrency issues in payment
    
    * fix: findProduct query
    
    * test: concurrency payment test
    
    * refactor: get productState
    
    * fix: transaction isolation level to READ_COMMITTED
    
    * chore: get product state
    
    * chore: delete unnecessary parts
    
    * chore: add cancel payment
    
    * chore: delete unused import

--- src/main/java/com/example/repick/domain/product/repository/ProductStateRepository.java
+++ src/main/java/com/example/repick/domain/product/repository/ProductStateRepository.java
@@ -4,8 +4,11 @@ import com.example.repick.domain.product.entity.ProductState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductStateRepository extends JpaRepository<ProductState, Long> {
 
     List<ProductState> findByProductId(Long id);
+
+    Optional<ProductState> findFirstByProductIdOrderByCreatedDateDesc(Long productId);
 }
--- src/main/java/com/example/repick/domain/product/service/PaymentService.java
+++ src/main/java/com/example/repick/domain/product/service/PaymentService.java
@@ -4,10 +4,8 @@ import com.example.repick.domain.product.dto.productOrder.GetProductOrderPrepara
 import com.example.repick.domain.product.dto.productOrder.PostPayment;
 import com.example.repick.domain.product.dto.productOrder.PostProductOrder;
 import com.example.repick.domain.product.entity.*;
-import com.example.repick.domain.product.repository.PaymentRepository;
-import com.example.repick.domain.product.repository.ProductCartRepository;
-import com.example.repick.domain.product.repository.ProductOrderRepository;
-import com.example.repick.domain.product.repository.ProductRepository;
+import com.example.repick.domain.product.repository.*;
+import com.example.repick.domain.product.validator.ProductValidator;
 import com.example.repick.domain.user.entity.User;
 import com.example.repick.domain.user.repository.UserRepository;
 import com.example.repick.global.error.exception.CustomException;
@@ -18,6 +16,7 @@ import com.siot.IamportRestClient.request.PrepareData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
@@ -35,7 +34,8 @@ public class PaymentService {
     private final ProductOrderRepository productOrderRepository;
     private final ProductCartRepository productCartRepository;
     private final IamportClient iamportClient;
-    private final ProductService productService;
+    private final ProductValidator productValidator;
+    private final ProductStateRepository productStateRepository;
 
     @Transactional
     public GetProductOrderPreparation prepareProductOrder(PostProductOrder postProductOrder) {
@@ -45,77 +45,81 @@ public class PaymentService {
         // 얼마 결제 예정인지 미리 등록해 결제 변조 원천 차단 (장바구니 합산, 할인된 가격 적용)
         String merchantUid = user.getProviderId() + System.currentTimeMillis();
         BigDecimal totalPrice = postProductOrder.productIds().stream()
-                .map(productId -> productRepository.findById(productId).orElseThrow(() -> new CustomException(INVALID_PRODUCT_ID)))
+                .map(productId -> {
+                    Product product = productRepository.findById(productId).orElseThrow(() -> new CustomException(INVALID_PRODUCT_ID));
+                    productValidator.validateProductState(productId, ProductStateType.SELLING);
+                    return product;
+                })
                 .map(product -> BigDecimal.valueOf(product.getDiscountPrice()))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
         PrepareData prepareData = new PrepareData(merchantUid, totalPrice);
         try{
-            // 사전 결제 검증 요청
-            iamportClient.postPrepare(prepareData);
-
-            // Payment 저장
-            Payment payment = Payment.of(user.getId(), merchantUid, totalPrice);
-            paymentRepository.save(payment);
-
-            // ProductOrder 저장
-            List<ProductOrder> productOrders  = postProductOrder.productIds().stream()
-                    .map(productId -> ProductOrder.of(user.getId(), productId, payment, ProductOrderState.DEFAULT))
-                    .toList();
-            productOrderRepository.saveAll(productOrders);
-
+            iamportClient.postPrepare(prepareData);  // 사전 결제 검증 요청
         } catch (Exception e) {
             throw new CustomException(e.getMessage(), INVALID_PREPARE_DATA);
         }
 
+        // Payment 저장
+        Payment payment = Payment.of(user.getId(), merchantUid, totalPrice);
+        paymentRepository.save(payment);
+
+        // ProductOrder 저장
+        List<ProductOrder> productOrders  = postProductOrder.productIds().stream()
+                .map(productId -> ProductOrder.of(user.getId(), productId, payment, ProductOrderState.DEFAULT))
+                .toList();
+        productOrderRepository.saveAll(productOrders);
+
         return GetProductOrderPreparation.of(merchantUid, totalPrice);
     }
 
-    @Transactional(noRollbackFor = CustomException.class)
+
+    @Transactional(isolation = Isolation.READ_COMMITTED, noRollbackFor = CustomException.class)
     public Boolean validatePayment(PostPayment postPayment){
         Payment payment = paymentRepository.findByMerchantUid(postPayment.merchantUid())
                 .orElseThrow(() -> new CustomException(INVALID_PAYMENT_ID));
+
+        // Pessimistic Lock - 이 트랜잭션이 끝날 때까지 다른 트랜잭션에서 해당 데이터(products)에 대한 접근을 막음
+        // 이미 처리된 주문인지 확인 (동시 결제 방지)
+        List<ProductOrder> productOrders = productOrderRepository.findByPayment(payment);
+        List<Product> products = productRepository.findAllByIdWithLock(productOrders.stream().map(ProductOrder::getProductId).toList());
+        products.forEach(product -> {
+            ProductState productState = productStateRepository.findFirstByProductIdOrderByCreatedDateDesc(product.getId())
+                    .orElseThrow(() -> new CustomException(PRODUCT_STATE_NOT_FOUND));
+            if(productState.getProductStateType() != ProductStateType.SELLING){
+                cancelPayment(payment);
+                throw new CustomException(PRODUCT_SOLD_OUT);
+            }
+        });
 
         // 이미 처리된 결제번호인지 확인
         if (paymentRepository.findByIamportUid(postPayment.iamportUid()).isPresent()) {
             throw new CustomException(DUPLICATE_PAYMENT);
         }
-        try{
-            com.siot.IamportRestClient.response.Payment paymentResponse = iamportClient.paymentByImpUid(postPayment.iamportUid()).getResponse();
 
-            // 가맹점 주문번호 일치 확인
-            if (!paymentResponse.getMerchantUid().equals(postPayment.merchantUid())) {
-                throw new CustomException(INVALID_PAYMENT_ID);
-            }
+        // 포트원 서버에서 결제 정보 가져오기
+        com.siot.IamportRestClient.response.Payment paymentResponse = getPaymentResponse(postPayment.iamportUid());
 
-            switch (paymentResponse.getStatus().toUpperCase()) {
-                case "READY", "CANCELLED", "FAILED"  -> {
-                    // 가상계좌 발급한다면 "READY" case 수정
-                    payment.completePayment(PaymentStatus.fromValue(paymentResponse.getStatus().toUpperCase()), postPayment.iamportUid(), postPayment.address());
-                    paymentRepository.save(payment);
-                    throw new CustomException(INVALID_PAYMENT_STATUS);
-                }
-                case "PAID" -> {
-                    // 결제금액 확인 (데이터베이스에 저장되어 있는 상품 가격과 비교)
-                    if (paymentResponse.getAmount().compareTo(payment.getAmount()) != 0) {
-                        CancelData cancelData = new CancelData(paymentResponse.getImpUid(), true);
-                        iamportClient.cancelPaymentByImpUid(cancelData);
-
-                        payment.updatePaymentStatus(PaymentStatus.CANCELLED);
-                        paymentRepository.save(payment);
-
-                        throw new CustomException(WRONG_PAYMENT_AMOUNT);
-                    }
-                }
-                default -> throw new CustomException(INVALID_PAYMENT_STATUS);
-            }
-
-        } catch (CustomException e){
-            throw e;
-        } catch (IamportResponseException | IOException e) {
+        // 가맹점 주문번호 일치 확인
+        if (!paymentResponse.getMerchantUid().equals(postPayment.merchantUid())) {
             throw new CustomException(INVALID_PAYMENT_ID);
-        } catch(Exception e){
-            throw new RuntimeException(e);
+        }
+
+        switch (paymentResponse.getStatus().toUpperCase()) {
+            case "READY", "CANCELLED", "FAILED"  -> {
+                // 가상계좌 발급한다면 "READY" case 수정
+                payment.completePayment(PaymentStatus.fromValue(paymentResponse.getStatus().toUpperCase()), postPayment.iamportUid(), postPayment.address());
+                paymentRepository.save(payment);
+                throw new CustomException(INVALID_PAYMENT_STATUS);
+            }
+            case "PAID" -> {
+                // 결제금액 확인 (데이터베이스에 저장되어 있는 상품 가격과 비교)
+                if (paymentResponse.getAmount().compareTo(payment.getAmount()) != 0) {
+                    cancelPayment(payment);
+                    throw new CustomException(WRONG_PAYMENT_AMOUNT);
+                }
+            }
+            default -> throw new CustomException(INVALID_PAYMENT_STATUS);
         }
 
         // 유효한 결제 -> Payment 저장
@@ -123,14 +127,33 @@ public class PaymentService {
         paymentRepository.save(payment);
 
         // ProductOrderState 업데이트, 장바구니 삭제, ProductSellingState 추가
-        List<ProductOrder> productOrders = productOrderRepository.findByPayment(payment);
         productOrders.forEach(productOrder -> {
             productOrder.updateProductOrderState(ProductOrderState.PAYMENT_COMPLETED);
             productCartRepository.deleteByUserIdAndProductId(productOrder.getUserId(), productOrder.getProductId());
-            productService.addProductSellingState(productOrder.getProductId(), ProductStateType.SOLD_OUT);
+            productStateRepository.save(ProductState.of(productOrder.getProductId(), ProductStateType.SOLD_OUT));
         });
+        productOrderRepository.saveAll(productOrders);
 
         return true;
+    }
+
+    private com.siot.IamportRestClient.response.Payment getPaymentResponse(String impUid) {
+        try {
+            return iamportClient.paymentByImpUid(impUid).getResponse();
+        } catch (IamportResponseException | IOException e) {
+            throw new CustomException(INVALID_PAYMENT_ID);
+        }
+    }
+
+    private void cancelPayment(Payment payment) {
+        CancelData cancelData = new CancelData(payment.getIamportUid(), true);
+        try {
+            iamportClient.cancelPaymentByImpUid(cancelData);
+        } catch (IamportResponseException | IOException e) {
+            throw new CustomException(INVALID_PAYMENT_ID);
+        }
+        payment.updatePaymentStatus(PaymentStatus.CANCELLED);
+        paymentRepository.save(payment);
     }
 
     @Transactional
@@ -150,6 +173,7 @@ public class PaymentService {
         return true;
     }
 
+    @Transactional
     public void addPointToSeller(ProductOrder productOrder){
         long profit = productOrder.getPayment().getAmount().longValue();
         if (profit >= 300000) {
@@ -173,6 +197,47 @@ public class PaymentService {
                 .getUser();
         seller.addPoint(profit);
         userRepository.save(seller);
+    }
+
+
+    // 동시성 테스트용 서비스 (iamportClient 통신 부분 제외)
+    @Transactional(isolation = Isolation.READ_COMMITTED, noRollbackFor = CustomException.class)
+    public Boolean validatePaymentForTest(PostPayment postPayment){
+        Payment payment = paymentRepository.findByMerchantUid(postPayment.merchantUid())
+                .orElseThrow(() -> new CustomException(INVALID_PAYMENT_ID));
+
+        List<ProductOrder> productOrders = productOrderRepository.findByPayment(payment);
+        List<Long> productIds = productOrders.stream().map(ProductOrder::getProductId).toList();
+
+        long startTime = System.currentTimeMillis();
+        System.out.println("Thread " + Thread.currentThread().getName() + " is trying to lock products: " + productIds);
+        List<Product> products = productRepository.findAllByIdWithLock(productIds);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Thread " + Thread.currentThread().getName() + " has locked products: " + productIds + ". Wait time: " + (endTime - startTime) + "ms");
+
+        products.forEach(product -> {
+            ProductState productState = productStateRepository.findFirstByProductIdOrderByCreatedDateDesc(product.getId())
+                    .orElseThrow(() -> new CustomException(PRODUCT_STATE_NOT_FOUND));
+            if(productState.getProductStateType() != ProductStateType.SELLING){
+                throw new CustomException(PRODUCT_SOLD_OUT);
+            }
+        });
+
+        if (paymentRepository.findByIamportUid(postPayment.iamportUid()).isPresent()) {
+            throw new CustomException(DUPLICATE_PAYMENT);
+        }
+
+        payment.completePayment(PaymentStatus.PAID, postPayment.iamportUid(), postPayment.address());
+        paymentRepository.save(payment);
+
+        productOrders.forEach(productOrder -> {
+            productOrder.updateProductOrderState(ProductOrderState.PAYMENT_COMPLETED);
+            productStateRepository.save(ProductState.of(productOrder.getProductId(), ProductStateType.SOLD_OUT));
+        });
+        productOrderRepository.saveAll(productOrders);
+
+        System.out.println("Thread " + Thread.currentThread().getName() + " has completed transaction.");
+        return true;
     }
 
 }
