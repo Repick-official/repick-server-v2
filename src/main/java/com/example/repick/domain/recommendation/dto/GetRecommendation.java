5db4a69144e9bf1204cb8feafa5b6e18758b4766
muhsroom1324 <mushroom1324@naver.com>
2024-06-16 15:12:33 +0900

    hotfix: distinct products

--- src/main/java/com/example/repick/domain/recommendation/dto/GetRecommendation.java
+++ src/main/java/com/example/repick/domain/recommendation/dto/GetRecommendation.java
@@ -9,10 +9,11 @@ public record GetRecommendation (
         @Schema(description = "상품명", example = "블랙 카라 오버핏 셔츠") String productName,
         @Schema(description = "상품 가격(할인 전)", example = "40000") Long price,
         @Schema(description = "할인 가격", example = "15000") Long discountPrice,
-        @Schema(description = "할인율",example = "30") Long discountRate
+        @Schema(description = "할인율",example = "30") Long discountRate,
+        @Schema(description = "브랜드 이름", example = "무인양품") String brandName
 ) {
 
     public GetRecommendation(Product product) {
-        this(product.getId(), product.getThumbnailImageUrl(), product.getProductName(), product.getPrice(), product.getDiscountPrice(), product.getDiscountRate());
+        this(product.getId(), product.getThumbnailImageUrl(), product.getProductName(), product.getPrice(), product.getDiscountPrice(), product.getDiscountRate(), product.getBrandName());
     }
 }
