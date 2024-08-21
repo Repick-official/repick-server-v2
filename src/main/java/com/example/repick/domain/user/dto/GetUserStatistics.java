957c7a5ad5590737a4a8953fa3944c1fd640a8e5
Jeonghyun Lee <hyeon00@ewhain.net>
2024-08-21 21:41:34 +0900

    fix: getUserStatistics

--- src/main/java/com/example/repick/domain/user/dto/GetUserStatistics.java
+++ src/main/java/com/example/repick/domain/user/dto/GetUserStatistics.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record GetUserStatistics(
         @Schema(name = "총 유저 수") long totalUserCount,
-        @Schema(name = "신규 가입 유저 수") long newUserCount
+        @Schema(name = "신규 가입 유저 수") long newUserCount,
+        @Schema(name = "여성 유저 비율") double femaleUserRatio,
+        @Schema(name = "남성 유저 비율") double maleUserRatio
 ){
-    public static GetUserStatistics of(long totalUserCount, long newUserCount) {
-        return new GetUserStatistics(totalUserCount, newUserCount);
+    public static GetUserStatistics of(long totalUserCount, long newUserCount, double femaleUserRatio, double maleUserRatio) {
+        return new GetUserStatistics(totalUserCount, newUserCount, femaleUserRatio, maleUserRatio);
     }
 }
