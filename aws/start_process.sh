3a7f68f5a220df471fdcb0e151c6a9fc25c4822d
muhsroom1324 <mushroom1324@naver.com>
2024-08-29 14:16:54 +0900

    feature: cicd test

--- aws/start_process.sh
+++ aws/start_process.sh
@@ -4,11 +4,10 @@ ECR_REPOSITORY="654654448479.dkr.ecr.ap-northeast-2.amazonaws.com/repick-repo"
 ECR_REPOSITORY="repick-repo"
 ECR_DOCKER_TAG="latest"
 
-aws ecr get-login-password --region ap-northeast-2 \
-  | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${ECR_REGISTRY}
 
-export IMAGE=${ECR_REPOSITORY};
-export TAG=${ECR_DOCKER_TAG};
-sudo docker pull ${{ env.ECR_REGISTRY }}/${{ env.ECR_REPOSITORY }}
-sudo docker run -d --name dc -p 8080:8080 ${{ env.ECR_REGISTRY }}/${{ env.ECR_REPOSITORY }}
+sudo docker pull ${ECR_REGISTRY}/${ECR_REPOSITORY}:${ECR_DOCKER_TAG}
+
+sudo docker run -d --name dc -p 8080:8080 ${ECR_REGISTRY}/${ECR_REPOSITORY}:${ECR_DOCKER_TAG}
+
 sudo docker image prune -f
