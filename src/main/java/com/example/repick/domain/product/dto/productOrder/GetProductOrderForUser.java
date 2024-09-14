cce67262ed0b522885bcb3625aa95b85af97a6f3
Jeonghyun Lee <hyeon00@ewhain.net>
2024-09-14 22:54:03 +0900

    hotfix: add orderDateTime

--- src/main/java/com/example/repick/domain/product/dto/productOrder/GetProductOrderForUser.java
+++ src/main/java/com/example/repick/domain/product/dto/productOrder/GetProductOrderForUser.java
@@ -4,8 +4,11 @@ import com.example.repick.domain.product.entity.Product;
 import com.example.repick.domain.product.entity.ProductOrder;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDateTime;
+
 public record GetProductOrderForUser(
-        @Schema(description = "주문 아이디") long productOrderId,
+        @Schema(description = "상품 주문 아이디") long productOrderId,
+        @Schema(description = "주문 일시") LocalDateTime orderDateTime,
         @Schema(description = "주문 상태") String orderState,
         @Schema(description = "브랜드 이름") String brandName,
         @Schema(description = "상품 이름") String productName,
@@ -16,6 +19,7 @@ public record GetProductOrderForUser(
     public static GetProductOrderForUser from(ProductOrder productOrder, Product product) {
         return new GetProductOrderForUser(
                 productOrder.getId(),
+                productOrder.getCreatedDate(),
                 productOrder.getProductOrderState().getValue(),
                 product.getBrandName(),
                 product.getProductName(),
