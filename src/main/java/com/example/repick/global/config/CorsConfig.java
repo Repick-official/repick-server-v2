655de4e72ad4fe149fedde749eea18a202cfe65f
Jeonghyun Lee <hyeon00@ewhain.net>
2024-10-21 19:29:41 +0900

    chore: add cors url

--- src/main/java/com/example/repick/global/config/CorsConfig.java
+++ src/main/java/com/example/repick/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.repick.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,12 +12,18 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${server.url}")
+    private String SERVER_URL;
+
+    @Value("${server.admin}")
+    private String ADMIN_URL;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://www.repick-server.shop", "https://repick-server.shop", "https://appleid.apple.com"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://repick-server.shop", "https://appleid.apple.com", SERVER_URL, ADMIN_URL));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
